6f7b72aec260cca60d937628fd61f76dc592266a
Nicholas Reilly <nick_reilly@outlook.com>
2025-04-14 09:45:20 -0400

    Added the main code for finding the job and added in the logger line
    Added in conditional stamemetns for the logger depending if something was found or not.

---
+++ src/main.py
@@ -0,0 +1,23 @@
+# src/main.py
+
+from job_scraper import scrape_jobs
+from emailer import send_email
+import logging
+
+def main():
+    try:
+        logging.basicConfig(level=logging.INFO, filename="logs/tracker.log", filemode="a",
+                            format="%(asctime)s - %(levelname)s - %(message)s")
+
+        jobs = scrape_jobs()
+        if jobs:
+            logging.info(f"Found {len(jobs)} job(s). Sending email...")
+            send_email(jobs)
+        else:
+            logging.info("No new jobs found.")
+
+    except Exception as e:
+        logging.error(f"Error in main: {e}")
+
+if __name__ == "__main__":
+    main()
